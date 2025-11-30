77123a45f61d4e5bd7e21ac45a59bed4ab49c698
Antuan <Totico388@hotmail.com>
2025-11-30 13:11:39 +0100

    Al fin listo

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -1,19 +1,3 @@
--- PLEASE READ THIS BEFORE RUNNING THE EXERCISE
-
--- ⚠️ IMPORTANT: This SQL file may crash due to two common issues: comments and missing semicolons.
-
--- ✅ Suggestions:
--- 1) Always end each SQL query with a semicolon `;`
--- 2) Ensure comments are well-formed:
---    - Use `--` for single-line comments only
---    - Avoid inline comments after queries
---    - Do not use `/* */` multi-line comments, as they may break execution
-
--- -----------------------------------------------
--- queries.sql
--- Complete each mission by writing your SQL query
--- directly below the corresponding instruction
--- -----------------------------------------------
 
 SELECT * FROM regions;
 SELECT * FROM species;
@@ -22,31 +6,51 @@ SELECT * FROM observations;
 
 
 -- MISSION 1
--- Your query here;
+
+-- 1. 
+SELECT * FROM observations LIMIT 10;
+
+-- 2. 
+SELECT DISTINCT region_id FROM observations;
+
+-- 3. 
+SELECT COUNT(DISTINCT species_id) AS total_species FROM observations;
+
+-- 4. 
+SELECT COUNT(*) AS observations_region_2 FROM observations WHERE region_id = 2;
+
+-- 5. 
+SELECT COUNT(*) AS observations_that_day 
+FROM observations 
+WHERE date = '1998-08-08';
 
 -- MISSION 2
--- Your query here;
+
+
+--6. 
+SELECT region_id, COUNT(*) AS total_obs FROM observations GROUP BY region_id ORDER BY total_obs DESC LIMIT 1;
+
+-- 7. 
+SELECT species_id, COUNT(*) AS total_obs FROM observations GROUP BY species_id ORDER BY total_obs DESC LIMIT 5;
+
+-- 8. 
+SELECT species_id, COUNT(*) AS total_obs FROM observations GROUP BY species_id HAVING COUNT(*) < 5;
+
+-- 9. 
+SELECT observer, COUNT(*) AS total_obs FROM observations GROUP BY observer ORDER BY total_obs DESC;
 
 
 -- MISSION 3
--- Your query here;
 
 
--- MISSION 4
--- Your query here;
+-- 10. 
+SELECT o.observation_id, r.name AS region_name FROM observations o JOIN regions r ON o.region_id = r.region_id;
+
+-- 11. 
+SELECT o.observation_id, s.scientific_name FROM observations o JOIN species s ON o.species_id = s.species_id;
+
+-- 12. 
+SELECT region_id, species_id, COUNT(*) AS total_obs FROM observations GROUP BY region_id, species_id ORDER BY region_id, total_obs DESC;
 
 
--- MISSION 5
--- Your query here;
 
-
--- MISSION 6
--- Your query here;
-
-
--- MISSION 7
--- Your query here;
-
-
--- MISSION 8
--- Your query here;
